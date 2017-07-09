f8888a1a6dfc8f2f4760ee7fc10f751f190fa042
Rajasekharan Vengalil <rajave@microsoft.com>
2017-07-09 15:08:31 -0700

    Make things buildable with GCC

--- inc/stddefs.h
+++ inc/stddefs.h
@@ -1,5 +1,16 @@
-#pragma once
+#ifndef STDDEFS_H
+#define STDDEFS_H
 
-#include <windows.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#ifndef min
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#ifndef max
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#endif // !STDDEFS_H
